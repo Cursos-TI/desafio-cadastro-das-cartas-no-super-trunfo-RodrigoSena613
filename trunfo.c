ba893ae964c52ff4430bfac426357887c5fb8f34
RodrigoSena613 <rodrigosena613@gmail.com>
2025-03-16 14:23:19 -0300

    Update trunfo.c

--- trunfo.c
+++ trunfo.c
@@ -1,7 +1,39 @@
-#include <stdio.h>
-
 int main(){
-    int populacao;
-    float area;
+    char estado;              
+    char codigo[5];           
+    char nome_cidade[MAX_CIDADE]; 
+    int populacao;            
+    float area;               
+    float pib;                
+    int pontos_turisticos;
+    
+    printf("Estado (letra de 'A' a 'H'): ");
+    scanf(" %c", &carta->estado);
+
+    printf("Código da Carta (ex: A01): ");
+    scanf("%s", carta->codigo);
+
+    printf("Nome da Cidade: ");
+
+    printf("Área (em km²): ");
+    scanf("%f", &carta->area);
+
+    printf("PIB: ");
+    scanf("%f", &carta->pib);
+
+    printf("Número de Pontos Turísticos: ");
+    scanf("%d", &carta->pontos_turisticos);
+
+printf("\nDados da Carta:\n");
+    printf("Estado: %c\n", carta.estado);
+    printf("Código da Carta: %s\n", carta.codigo);
+    printf("Nome da Cidade: %s\n", carta.nome_cidade);
+    printf("População: %d\n", carta.populacao);
+    printf("Área: %.2f km²\n", carta.area);
+    printf("PIB: %.2f\n", carta.pib);
+    printf("Pontos Turísticos: %d\n", carta.pontos_turisticos);
+}
+
+
 
 
